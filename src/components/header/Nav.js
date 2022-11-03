0304711a8515eb5f42bdfc9f27cd140ac6dbca6f
5iddesh <siddeshs083@gmail.com>
2022-11-03 13:47:06 +0530

    c3

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -27,15 +27,27 @@ export default function Nav() {
     const navMenu = document.querySelector(".nav-menu");
     const login = document.querySelector(".login");
 
-    if(navMenu.style.display !== "flex"){
-      navMenu.style.display = "flex";
-      login.style.display = "inline-block";
+    if(navMenu.style.opacity != "1"){
+      navMenu.style.opacity = "1";
+      login.style.opacity = "1";
     }else{
-      navMenu.style.display = "none";
-      login.style.display = "none";
+      navMenu.style.opacity = "0";
+      login.style.opacity = "0";
     }
-   
   }
+
+  // const handleClick = (e) => {
+  //   const navMenu = document.querySelector(".nav-menu");
+  //   const login = document.querySelector(".login");
+
+  //   if(navMenu.style.display !== "flex"){
+  //     navMenu.style.display = "flex";
+  //     login.style.display = "inline-block";
+  //   }else{
+  //     navMenu.style.display = "none";
+  //     login.style.display = "none";
+  //   }
+  // }
 
   return (
     <nav className="nav">
@@ -55,19 +67,9 @@ export default function Nav() {
         </div>
         <img onClick={(e) => handleClick(e)} src='assets/menu-01_generated.jpg' alt="hamburger-logo" className='hamburger-menu'/>
 
-        {/* <div className='mobile-view'>
-          <a href='#pricing'>Pricing</a>
-          
-          <a href='#deviceSolutions'>deviceSolutions</a>
-
-          <a href='resources'>Resources</a>
-          <button>signIn</button>
-        </div> */}
+      
     </nav>
   )
 }
 
 
-  // <useReference.Provider value={deviceSolutions} > 
-  // <span onClick={() => deviceSolutions.current.scrollIntoView() }>deviceSolutions</span>
-  // </useReference.Provider> 
